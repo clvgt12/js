17c22769280be1bf4a898fd48a2a6ca491d3e19c
Chris <15834506+clvgt12@users.noreply.github.com>
2020-08-10 08:01:40 -0400

    Update whatBrowser.js

--- whatBrowser.js
+++ whatBrowser.js
@@ -1,24 +1,24 @@
 var whatBrowser = {
   // Opera 8.0+
-  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  isOpera : function(){ return((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;);}
 
   // Firefox 1.0+
-  var isFirefox = typeof InstallTrigger !== 'undefined';
+  isFirefox : function(){ return(typeof InstallTrigger !== 'undefined';);}
 
   // Safari 3.0+ "[object HTMLElementConstructor]" 
-  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+  isSafari : function(){return(/constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)););}
 
   // Internet Explorer 6-11
-  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  isIE : function(){return(/*@cc_on!@*/false || !!document.documentMode;);}
 
   // Edge 20+
-  var isEdge = !isIE && !!window.StyleMedia;
+  isEdge : function(){return( !isIE && !!window.StyleMedia; );}
 
   // Chrome 1+
-  var isChrome = !!window.chrome;
+  isChrome : function(){return( !!window.chrome; );}
 
   // Blink engine detection
-  var isBlink = (isChrome || isOpera) && !!window.CSS;
+  var isBlink = function(){return((isChrome || isOpera) && !!window.CSS;);}
   console.log("Opera - " + isOpera);
   console.log("Firefox - " + isFirefox);
   console.log("Safari - " + isSafari);
