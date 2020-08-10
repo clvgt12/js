9ff0e93a32bb64047cc737ed9b2056a5dcd73945
Chris <15834506+clvgt12@users.noreply.github.com>
2020-08-10 14:30:27 -0400

    Update whatBrowser.js

--- whatBrowser.js
+++ whatBrowser.js
@@ -1,52 +1,64 @@
-var whatBrowser = new Object();
+//
+// whatBrowser JS object
+// This object defines methods that determine which Browser the JS is executing under
+// The general consensus is this is a worthless exercise, but whatever.
+//
+
+function whatBrowser() {
+
 	// Opera 8.0+
-	whatBrowser.isOpera=function() {
-	    var r = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-	    return r;
+	this.isOpera = function() {
+	  var r = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+	  return r;
 	};
 
 	// Firefox 1.0+
-	whatBrowser.isFirefox= function() {
-	    var r = typeof InstallTrigger !== 'undefined';
-	    return r;
+	this.isFirefox = function() {
+	  var r = typeof InstallTrigger !== 'undefined';
+	  return r;
 	};
 
 	// Safari 3.0+ "[object HTMLElementConstructor]" 
-	whatBrowser.isSafari= function() {
-	    var r = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-	    return r;
+	this.isSafari = function() {
+	  var r = /constructor/i.test(window.HTMLElement) || (function(p) {
+		 return p.toString() === "[object SafariRemoteNotification]";
+	  })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+	  return r;
 	};
 
 	// Internet Explorer 6-11
-	whatBrowser.isIE= function() {
-	    var r = /*@cc_on!@*/false || !!document.documentMode;
-	    return r;
+	this.isIE = function() {
+		var r = /*@cc_on!@*/false || !!document.documentMode;
+		return r;
 	};
 
 	// Edge 20+
-	whatBrowser.isEdge= function() {
-		var r = !isIE && !!window.StyleMedia;
-		return r;
+	this.isEdge = function() {
+	  var r = !this.isIE() && !!window.StyleMedia;
+	  return r;
 	};
 
 	// Chrome 1+
-	whatBrowser.isChrome= function() {
-		var r = !!window.chrome;
-		return r;
+	this.isChrome = function() {
+	  var r = !!window.chrome;
+	  return r;
 	};
 
 	// Blink engine detection
-	whatBrowser.isBlink= function() {
-		var r = (isChrome || isOpera) && !!window.CSS;
-		return (r);
+	this.isBlink = function() {
+	  var r = (this.isChrome() || this.isOpera()) && !!window.CSS;
+	  return (r);
 	};
 
-	whatBrowser.debug= function() {
-		console.log("Opera - " + this.isOpera());
-		console.log("Firefox - " + this.isFirefox());
-		console.log("Safari - " + this.isSafari());
-		console.log("IE - " + this.isIE());
-		console.log("Edge - " + this.isEdge());
-		console.log("Chrome - " + this.isChrome());
-		console.log("Blink - " + this.isBlink());
+	this.debug = function() {
+	  console.log("Opera - " + this.isOpera());
+	  console.log("Firefox - " + this.isFirefox());
+	  console.log("Safari - " + this.isSafari());
+	  console.log("IE - " + this.isIE());
+	  console.log("Edge - " + this.isEdge());
+	  console.log("Chrome - " + this.isChrome());
+	  console.log("Blink - " + this.isBlink());
 	};
+}
+
+
